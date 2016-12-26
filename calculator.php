ce120698ddc02392b62c473de807591be6a8c59c
mami-sugi <mami.no_suke@icloud.com>
2016-12-26 21:01:40 +0900

    Create calculator.php

---
+++ calculator.php
@@ -0,0 +1,62 @@
+<?php
+mb_language('ja');
+mb_internal_encoding('UTF-8');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>電卓</title>
+</head>
+<body>
+<form action="" method="post">
+    <h2 align="center">Please input calculation</h2>
+    <p align="center">
+        <input type="text" name="first" size="5">
+        <select name="str" size="1">
+            <option value=""></option>
+            <option value="+">+</option>
+            <option value="-">-</option>
+            <option value="×">×</option>
+            <option value="÷">÷</option>
+         <input type="text" name="second" size="5">
+        <br>
+        <input type="submit" value="計算"><input type="reset" value="リセット">
+    </p>
+</form>
+<hr>
+<p align="center">
+<?php
+if(empty($_POST['first']) || empty($_POST['second'])) {//check undifined
+    echo "please input number";
+}elseif(empty($_POST['str'])){
+    echo "please select operator symbol";
+}else{
+    if(ctype_digit($_POST['first']) && ctype_digit($_POST['second'])) {
+        $var1 = (int)$_POST['first'];
+        $var2 = $_POST['str'];
+        $var3 = (int)$_POST['second'];
+        if ($var2 == '+') {
+            $result = $var1 + $var3;
+            echo $var1 . $var2 . $var3 . "=" . $result;
+        } elseif ($var2 == '-') {
+            $result = $var1 - $var3;
+            echo $var1 . $var2 . $var3 . "=" . $result;
+        } elseif ($var2 == '×') {
+            $result = $var1 * $var3;
+            echo $var1 . $var2 . $var3 . "=" . $result;
+        } elseif ($var2 == '÷') {
+            if ($var3 == 0) {
+                print_r("Can't devided by 0!!");
+            } else {
+                $result = $var1 / $var3;
+                echo $var1 . $var2 . $var3 . "=" . $result;
+            }
+        }
+    }else{
+        echo "You can input only number!!";
+    }
+}
+?>
+</p>
+</body>
+</html>
